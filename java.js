3769e2bce2189db2c50421c871b51bb7bb06188e
muathshabatat <shabatatmuath@gmail.com>
2019-10-02 02:00:33 +0300

    add

---
+++ java.js
@@ -0,0 +1,13 @@
+
+
+var property = prompt("Do you have a Mercedes car?");
+if(property == 'yes'){
+   alert("you are lucky");
+}else if(property == 'no'){
+   alert("I hope you have a one soon!");
+}
+else {
+    alert("sorry try again!");
+}
+
+
